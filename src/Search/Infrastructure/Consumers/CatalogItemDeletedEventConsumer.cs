9eebcd5769885d21d8e4c17dd6e25bc63d26d1fb
elahe471 <khorasani.elaheh@gmail.com>
2026-08-24 19:10:25 +0200

    Update Catalog.Contracts and add CatalogItemDeletedEventConsumer
    Updated Catalog.Contracts package to v1.0.3 in Search.csproj.
    Added CatalogItemDeletedEventConsumer to handle CatalogItemDeletedEvent messages and remove items from Elasticsearch by slug.

---
+++ src/Search/Infrastructure/Consumers/CatalogItemDeletedEventConsumer.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.Contracts.IntegrationEvents;
+
+namespace Search.Infrastructure.Consumers
+{
+    public class CatalogItemDeletedEventConsumer(ElasticsearchClient elasticsearchClient) : IConsumer<CatalogItemDeleteEvent>
+    {
+        private readonly ElasticsearchClient _elasticsearchClient = elasticsearchClient;
+
+        public async Task Consume(ConsumeContext<CatalogItemDeletedEvent> context)
+        {
+            var message = context.Message;
+
+            if (message is null) return;
+
+            var response = await _elasticsearchClient.DeleteAsync(
+           CatalogItemIndex.IndexName,
+           message.Slug,
+           context.CancellationToken);
+        }
+    }
+}
